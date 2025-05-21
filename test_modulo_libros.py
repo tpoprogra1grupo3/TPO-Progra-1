3d5b4f4a1946d6680d3cd6951a371d0d2036b479
elianbineder <elianbineder@gmail.com>
2025-05-21 16:37:59 -0300

    Agregado de pruebas unitarias para el modulo_libros

---
+++ test_modulo_libros.py
@@ -0,0 +1,20 @@
+from modulo_libros import biblioteca
+
+def test_biblioteca_no_vacia():  #Testea que la biblioteca contenga elementos
+    libros = biblioteca()
+    assert isinstance (libros, list)
+    assert len (libros) == 10
+    
+def test_primer_libro(): #Testea primer libro en la biblioteca
+    libros = biblioteca()
+    assert libros [0] ==  ['Cien años de soledad', 'L001', 'Gabriel García Márquez', 5, 'Sudamericana']
+
+def test_libro_individual(): #Testea que los libros se encuentren correctamente clasificados
+    libros = biblioteca()
+    for libro in libros:
+        assert isinstance (libros, list)
+        assert len (libro) == 5
+
+def test_ultimo_libro(): #Testea ultimo libro en la biblioteca
+    libros = biblioteca()
+    assert libros [9] ==  ['El código Da Vinci', 'L010', 'Dan Brown', 1, 'Planeta']
